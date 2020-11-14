1c19611bb7a4f4af2de00a081a151c5a6b318b5c
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2020-11-14 18:15:56 +0100

    Essaie
    Essaie pour défilement et séparation sous titre

---
+++ ProjetST/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProjetST
+{
+    class MainClass
+    {
+        public static async Task Main()
+        {
+            ST s = new ST();
+
+            Task t = s.PrintTextWithDelay();
+
+            await t;
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ ProjetST/ST.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProjetST
+{
+    public class ST
+    {
+        public void LireST()
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            StreamReader file =
+                new StreamReader(@"/Users/chloemerck/Desktop/Glee - S02E02 - Britney-Brittany copie.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+
+            
+        }
+
+       public async Task PrintWithDelay(String s)
+        {
+            for(int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+                await Task.Delay(5);
+
+            }
+        }
+
+        public async Task PrintTextWithDelay()
+        {
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            using(StreamReader outputFile= new StreamReader(mydocpath + @"/Glee - S02E02 - Britney-Brittany copie.txt"))
+            {
+                string s;
+                while ((s = outputFile.ReadLine()) != null)
+                {
+                    Task t = PrintWithDelay(s);
+                    await t;
+                }
+            }
+           
+        }
+
+            public ST()
+        {
+        }
+    }
+}
